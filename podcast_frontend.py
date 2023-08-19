f93cba8eef6c2e53d7fadc66106af6510b36b3e7
eelay234 <eelay234@gmail.com>
2023-08-18 17:25:55 -0700

    Add files via upload

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -65,45 +65,47 @@ def main():
     if process_button:
         placeholder = st.empty()
 
-        # Call the function to process the URLs and retrieve podcast guest information
-        podcast_info = process_podcast_info(url)
+        with st.empty():
 
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
+            # Call the function to process the URLs and retrieve podcast guest information
+            podcast_info = process_podcast_info(url)
 
-        # Display the podcast title
-        st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
+            # Right section - Newsletter content
+            st.header("Newsletter Content")
 
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
+            # Display the podcast title
+            st.subheader("Episode Title")
+            st.write(podcast_info['podcast_details']['episode_title'])
 
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
+            # Display the podcast summary and the cover image in a side-by-side layout
+            col1, col2 = st.columns([7, 3])
 
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+            with col1:
+                # Display the podcast episode summary
+                st.subheader("Podcast Episode Summary")
+                st.write(podcast_info['podcast_summary'])
 
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
+            with col2:
+                st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
+            # Display the podcast guest and their details in a side-by-side layout
+            col3, col4 = st.columns([3, 7])
+
+            with col3:
+                st.subheader("Podcast Guest")
+                st.write(podcast_info['podcast_guest']['name'])
 
 
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            with col4:
+                st.subheader("Podcast Guest Details")
+                st.write(podcast_info["podcast_guest"]['summary'])
 
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+            # Display the five key moments
+            st.subheader("Key Moments")
+            key_moments = podcast_info['podcast_highlights']
+            for moment in key_moments.split('\n'):
+                st.markdown(
+                    f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
